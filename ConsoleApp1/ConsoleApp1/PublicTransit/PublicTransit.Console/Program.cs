028e56d696cdf0d672519bebbfa2772adcf8d8d0
smobi916 <ko2295805@gmail.com>
2025-04-04 13:00:22 +0300

    Додано файли Lab1

---
+++ ConsoleApp1/ConsoleApp1/PublicTransit/PublicTransit.Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1.PublicTransit.PublicTransit.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.PublicTransit.PublicTransit.Console
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var busService = new CrudService<Bus>();
+
+            // Створення кількох автобусів
+            Bus bus1 = new Bus("Mercedes", "Sprinter", 30, "Route A", 10.5);
+            var bus2 = new Bus("Volvo", "9400", 50, "Route B", 15.2);
+
+            busService.Create(bus1);
+            busService.Create(bus2);
+
+            // Виведення всіх автобусів
+            object value = Console.WriteLine("All Buses:");
+            foreach (var bus in busService.ReadAll())
+            {
+                bus.DisplayDetails();
+            }
+
+            // Збереження даних у файл
+            busService.Save("buses.json");
+
+            // Завантаження з файлу
+            var loadedService = new CrudService<Bus>();
+            loadedService.Load("buses.json");
+
+            object value1 = Console.WriteLine("\nLoaded Buses:");
+            foreach (var bus in loadedService.ReadAll())
+            {
+                bus.DisplayDetails();
+            }
+        }
+    }
+}
