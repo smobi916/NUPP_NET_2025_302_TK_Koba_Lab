b902036fdc8ca265129cab19fc88c1ac0fd22aed
smobi916 <128784284+smobi916@users.noreply.github.com>
2025-06-04 17:05:47 +0300

    Add files via upload

---
+++ WarShips/WarShips.Common/Destroyer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WarShips.Common
+{
+    internal class Destroyer : Ship
+    {
+        public int NumberOfTorpedos { get; set; }
+        public Boolean Mines { get; set; }
+        public int NumberDeepBomb { get; set; }
+
+        public Destroyer(string name, int hp, int armor)
+           : base(name, hp, armor) { }
+
+        public void LaunchTorpedo()
+        {
+            Console.WriteLine($"{name} запускає торпеду!");
+        }
+    }
+}
---
+++ WarShips/WarShips.Common/ShipExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarShips.Common
+{
+    public static class TroopersExtensions
+    {
+    }
+    public static class ShipExtensions
+    {
+        // Метод розширення
+        public static void Signal(this Ship ship)
+        {
+            Console.WriteLine(" Корабель подає сигнал тривоги!");
+        }
+    }
+}
